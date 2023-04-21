c22e4fcfbf6a8a0ba3d0eca617aea5dac294381f
ILYAisi <131413889+ILYAisi@users.noreply.github.com>
2023-04-21 10:43:41 +0900

    Add files via upload

---
+++ Mnogoulnic2/Mnogoulnic/Mnogougolnic.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace Mnogoulnic
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BTN_Close = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BTN_Close
+            // 
+            this.BTN_Close.Location = new System.Drawing.Point(206, 198);
+            this.BTN_Close.Name = "BTN_Close";
+            this.BTN_Close.Size = new System.Drawing.Size(119, 40);
+            this.BTN_Close.TabIndex = 0;
+            this.BTN_Close.Text = "Close";
+            this.BTN_Close.UseVisualStyleBackColor = false;
+            this.BTN_Close.Click += new System.EventHandler(this.BTN_Close_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Maroon;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BTN_Close);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Person Class";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BTN_Close;
+    }
+}
+
---
+++ Mnogoulnic2/Mnogoulnic/Mnogougolnic.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mnogoulnic
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void BTN_Close_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            // 1. Объявить экземпляр типа GraphicsPath - класс биб-лиотеки .NET Framework,
+            // который устанавливает последовательность соединенных линий и кривых.
+            System.Drawing.Drawing2D.GraphicsPath gp = new
+                System.Drawing.Drawing2D.GraphicsPath();
+
+            // 2. Создать массив точек, соответствующих координатам
+            //    пятиугольника, образовывающего форму.
+            // 2.1. Объявить экземпляр типа "массив точек Point[]".
+            //    Point - это класс, который описывает точку на экране.
+            Point[] mp = new Point[10];
+
+            // 2.2. Выделить память для каждой точки и заполнить
+            //         ее значениями.
+            mp[0] = new Point(340, 0);
+            mp[1] = new Point(180, 138);
+            mp[2] = new Point(61, 151);
+            mp[3] = new Point(169, 225);
+            mp[4] = new Point(98, 311);
+            mp[5] = new Point(255, 263);
+            mp[6] = new Point(400, 327);
+            mp[7] = new Point(353, 187);
+            mp[8] = new Point(455, 95);
+            mp[9] = new Point(338, 82);
+
+            // 3. Добавить массив точек Point[] в экземпляр gp
+            gp.AddPolygon(mp);
+
+            // 4. Создать область (Region) на основе последователь-ности точек gp
+            Region rg = new Region(gp);
+
+            // 5. Установить область формы this.Region
+            //       в новое значение rg
+            this.Region = rg;
+
+        }
+    }
+}
